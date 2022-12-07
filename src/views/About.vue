40f886fec97623547af82d28afc21a6cafdd1a97
Jonathan Potter <jonathan.d.potter@outlook.com>
2022-12-07 18:45:14 -0500

    Updated about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="container w-75">
     <h2>About</h2>
+    <p>
+      This app is an easy way to make a word document for job dialogue feedback.
+      All a user has to do is answer the questions and download the finished
+      document.
+    </p>
     <p>This app is written in TypeScript with Vue.js. There is no back-end.</p>
     <p>
-      It uses Vuex for state management, Vue Router for routing, dockx.js and
+      It uses Vuex for state management, Vue Router for routing, docx.js and
       file-saver to create a .docx document for download, Bootstrap and SASS for
       styling and SweetAlert for some fun alerts.
     </p>
